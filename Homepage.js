575e389ee735433b63f92c746dcbb8ea400a8775
MacRJ <rmacwj@gmail.com>
2017-07-20 08:41:54 -0700

    about to start combining pages. Next need to build object, and start condition

---
+++ Homepage.js
@@ -0,0 +1,62 @@
+window.addEventListener("load", function() {
+  console.log("i'm up");
+
+
+
+
+
+  d3.select("body")
+    .style("background-color", "black")
+  //
+  d3.select("#greeting").transition()
+    .style("background-color", "white")
+    .style("height", "400px")
+    .style("margin-top", "300px")
+
+
+
+
+
+
+
+})
+
+function dropDown() {
+
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function dropDown1() {
+
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
